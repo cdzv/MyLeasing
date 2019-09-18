6878bd461479a95abf946bcb66c11c373851c6c0
cdzv <cristian.danilo222@gmail.com>
2019-09-18 11:21:58 -0500

    Correction (API -> Add -> PropertyTypesController)

---
+++ MyLeasing.Web/Controllers/API/PropertyTypesController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyLeasing.Web.Data;
+using MyLeasing.Web.Data.Entities;
+
+namespace MyLeasing.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class PropertyTypesController : Controller
+    {
+        private readonly DataContext _dataContext;
+
+        public PropertyTypesController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<PropertyType> GetPropertyTypes()
+        {
+            return _dataContext.PropertyTypes.OrderBy(pt => pt.Name);
+        }
+    }
+}
